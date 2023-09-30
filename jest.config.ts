14b7a3e8256f3b41b24b3d81121692cc8420f0e8
Allan <allanjunior1997@gmail.com>
2023-09-29 22:47:25 -0300

    fix: jest

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,7 @@ module.exports = {
     preset: 'ts-jest',
     testIgnorePatterns: ["/node_modules/"],
     setupFilesAfterEnv: [
-        "<rootDir>/src/tests/setuptests.ts"
+        "<rootDir>/src/tests/setupTests.ts"
     ],
     transform: {
         "ˆ.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest"
