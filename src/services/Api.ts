91f61b02cda0d05103f2e787b06520a83035ed4c
Allan Alves <allanjunior1997@gmail.com>
2023-10-22 12:46:25 -0300

    refactor: identacao

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const Api = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL,
-    timeout: Number(import.meta.env.VITE_API_TIMEOUT),
-  })
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  timeout: Number(import.meta.env.VITE_API_TIMEOUT),
+})
 
 export default Api;
